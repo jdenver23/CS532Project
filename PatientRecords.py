9248f558b21cfbbf042e5068af8bbc04b5dc8318
jdenver23 <denverjacob23@gmail.com>
2022-11-07 20:45:50 -0800

    Electronic Patient Record update 11/7/2022

--- PatientRecords.py
+++ PatientRecords.py
@@ -36,6 +36,13 @@ MEDICAL ENCOUNTERS:
     Referral to Specialist
     Recommended Follow-up
 """
+import csv
+import tkinter as tk
+import EditPatient
+import os
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
 class Patient:
 
@@ -110,3 +117,72 @@ class Patient:
 
         def set_current_appointments(self, x):
             self.current_appointments = x
+
+def runGui(person):
+    root = tk.Tk()
+
+    root.title("Patient Record")
+
+    top = Frame(root)
+    bottom = Frame(root)
+    top.pack(side=TOP)
+    bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+    listbox = Listbox(root, height = 500,
+                    width = 500,
+                    bg = "grey",
+                    activestyle = 'dotbox',
+                    font = "Helvetica",
+                    fg = "black")
+
+    root.geometry("500x500") 
+    
+    listbox.insert(1, "Patient Name: " + person.patient_name)
+    listbox.insert(2, "")
+    listbox.insert(3, "Phone Number: " + person.phone_number)
+    listbox.insert(4, "")
+    listbox.insert(5, "Address: " + person.address)
+    listbox.insert(6, "")
+    listbox.insert(7, "Insurance Carrier: " + person.insurance_carrier)
+    listbox.insert(8, "")
+    listbox.insert(9, "Date of Birth: " + person.date_of_birth)
+    listbox.insert(10, "")
+    listbox.insert(11, "Gender: " + person.gender)
+    listbox.insert(12, "")
+    listbox.insert(13, "Primary Care Physician: " + person.primary_care_physician)
+    listbox.insert(14, "")
+    listbox.insert(15, "Current Medications: " + person.current_meds)
+    listbox.insert(16, "")
+    listbox.insert(17, "Appointments: " + person.current_appointments)
+    listbox.pack()
+
+    home_button = ttk.Button(root, text='Home')
+    home_button.pack(in_=top, side=LEFT)
+
+    def edit_entry():
+        root.destroy()
+        EditPatient.enter_prog(person.id_number)
+
+    edit_button = ttk.Button(root, text='Edit Information', command = edit_entry)
+    edit_button.pack(in_=top, side=LEFT)
+
+    def print_file():
+        file_to_print = filedialog.askopenfilename(
+        initialdir="/", title="Select file", 
+        filetypes=(("Text files", "*.txt"), ("all files", "*.*")))
+      
+        if file_to_print:
+            os.ShellExecute(0, "print", file_to_print, None, ".", 0)
+
+    save_button = ttk.Button(root, text="Save Page", command=print_file)
+    save_button.pack(in_=top, side=RIGHT)
+    
+    root.mainloop()
+
+def initialize(passed_id_number):
+    with open("users.csv", 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            if row[0] == passed_id_number:
+                instance = Patient(row[0], row[1] + " " + row[2], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12])
+                runGui(instance)
