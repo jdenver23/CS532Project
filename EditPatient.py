a74f90026a486b3d625c9f53770b9295f5315aac
lindsayly <lindsay.ly1225@gmail.com>
2022-11-13 16:51:05 -0800

    update formatting
    formatting how information is inputted into the users.csv file

--- EditPatient.py
+++ EditPatient.py
@@ -21,6 +21,7 @@ def runGui(id_number):
     
     first_name_var=tk.StringVar()
     last_name_var=tk.StringVar()
+    email_var = tk.StringVar()
     phone_var=tk.StringVar()
     address_var=tk.StringVar()
 
@@ -30,9 +31,11 @@ def runGui(id_number):
         for r in df.index:
             if(df["ID"][r].astype(str) == id_number):
                 if(first_name_var.get() != ""):
-                    df.at[r, "First Name"] = first_name_var.get()
+                    df.at[r, "First Name"] = first_name_var.get().strip().upper()
                 if(last_name_var.get() != ""):
-                    df.at[r, "Last Name"] = last_name_var.get()
+                    df.at[r, "Last Name"] = last_name_var.get().strip().upper()
+                if(email_var.get() != ""):
+                    df.at[r, "Email"] = email_var.get().strip().lower()
                 if(phone_var.get() != ""):
                     df.at[r, "Phone Number"] = phone_var.get()
                 if(address_var.get() != ""):
@@ -40,32 +43,38 @@ def runGui(id_number):
         df.to_csv("users.csv", index=False)
 
     first_name_label = tk.Label(top, text = 'First Name', font=('calibre',10, 'bold'))
-    
     first_name_entry = tk.Entry(top, textvariable = first_name_var, font=('calibre',10,'normal'))
     
     last_name_label = tk.Label(top, text = 'Last Name', font = ('calibre',10,'bold'))
-    
     last_name_entry=tk.Entry(top, textvariable = last_name_var, font = ('calibre',10,'normal'))
 
-    phone_label = tk.Label(top, text = 'Phone Number', font=('calibre',10, 'bold'))
-    
-    phone_entry = tk.Entry(top, textvariable = phone_var, font=('calibre',10,'normal'))
+    email_label = tk.Label(top, text = 'Email', font = ('calibre',10,'bold'))
+    email_entry=tk.Entry(top, textvariable = email_var, font = ('calibre',10,'normal'))
+
+    phone_label = tk.Label(top, text = 'Phone Number', font =('calibre',10, 'bold'))
+    phone_entry = tk.Entry(top, textvariable = phone_var, font =('calibre',10,'normal'))
     
     address_label = tk.Label(top, text = 'Address', font = ('calibre',10,'bold'))
-    
     address_entry=tk.Entry(top, textvariable = address_var, font = ('calibre',10,'normal'))
     
     sub_btn=tk.Button(top,text = 'Submit', command = submit)
     
-    first_name_label.grid(row=1,column=0,pady=5)
-    first_name_entry.grid(row=1,column=1,pady=5)
-    last_name_label.grid(row=2,column=0,pady=5)
-    last_name_entry.grid(row=2,column=1,pady=5)
-    phone_label.grid(row=3,column=0,pady=5)
-    phone_entry.grid(row=3,column=1,pady=5)
-    address_label.grid(row=4,column=0,pady=5)
-    address_entry.grid(row=4,column=1,pady=5)
-    sub_btn.grid(row=5,column=0,pady=5)
+    first_name_label.grid(row = 1, column = 0, pady = 5)
+    first_name_entry.grid(row = 1, column = 1, pady = 5)
+
+    last_name_label.grid(row = 2, column = 0, pady = 5)
+    last_name_entry.grid(row = 2, column = 1, pady = 5)
+
+    email_label.grid(row = 3, column = 0, pady = 5)
+    email_entry.grid(row = 3, column = 1, pady = 5)
+
+    phone_label.grid(row = 4, column = 0, pady = 5)
+    phone_entry.grid(row = 4, column = 1, pady = 5)
+
+    address_label.grid(row = 5, column = 0, pady = 5)
+    address_entry.grid(row = 5, column = 1, pady = 5)
+    
+    sub_btn.grid(row = 6, column = 0, pady = 5)
 
     top.mainloop()
 
